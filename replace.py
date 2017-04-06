93c8619bb2b08a299e21a6501b63821822250e95
Sam Bender <rednebmas@gmail.com>
2017-04-06 10:01:15 -0700

    Pretty output

--- replace.py
+++ replace.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import sys
-import glob
+import re
+import os
+from binaryornot.check import is_binary
 
 # support renaming files as well
 
@@ -22,11 +24,11 @@ Options:
   -t or --test: don't perform replacements, just output matches and replacements
 
 Usage:
-  replace search_pattern replace_pattern file1 file2 file3 ...
-  find . | replace search_pattern replace_pattern -t
+  replace "search_pattern" "replace_pattern" file1 file2 file3 ...
+  find . | replace "search_pattern" "replace_pattern" -t
 
 Examples:
-  replace "hello" "HELLO THERE" "*"
+  replace "hello" "HELLO THERE" *
 
 Regex cheat sheet:
   Characters that need to be escaped: *\.^$+?[]()|
@@ -44,38 +46,39 @@ print('sys.argv = ' + str(sys.argv))
 
 files_start_index = 2 + len(options) + 1
 files = sys.argv[files_start_index:]
-print('files')
-print(files)
-
-print('\nsearch pattern: ' + search_pattern)
-
-import re
-import os
+print('files = ' + str(files) + '\n')
 
 compiled_pattern = re.compile(search_pattern)
 
+arrow_color = '\033[91m'
+bold_format = '\033[1m'
+end_format = '\033[0m'
+
 for fpath in files:
-    if os.path.isfile(fpath) is False or '.git' in fpath: 
+    if os.path.isfile(fpath) is False or '.git' in fpath or fpath.endswith('.swp'): 
         continue
 
     with open(fpath, 'r') as f:
         contents = f.read()
 
-    print('here')
-
     if compiled_pattern.search(contents):
-        print('matched')
-        print("<" + fpath + ">")
-        with open(fpath, 'w') as f:
-            # write to file if not in test mode
-            if '-t' not in options and '--test' not in options:
+        if is_binary(fpath):
+            print('Skipping matches for binary file <{0}>'.format(fname))
+            continue
+
+        print(bold_format + "<" + fpath + ">" + end_format)
+
+        # print out the replacements
+        matches = compiled_pattern.findall(contents)
+        for match in matches:
+            replacement = re.sub(compiled_pattern, replace_pattern, match)
+            print('{} {}=>{} {}'.format(match, arrow_color + bold_format, end_format, replacement))
+
+        # perform the replacement if not in test mode
+        if '-t' not in options and '--test' not in options:
+            with open(fpath, 'w') as f:
                 f.write(re.sub(compiled_pattern, replace_pattern, contents))
 
-            # print out the replacements
-            matches = re.search(compiled_pattern, contents)
-            for match in matches.groups():
-                print("Match: " + match)
-                print("Replacement: " + re.sub(compiled_pattern, replace_pattern, match))
 
 
 
